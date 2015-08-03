60c2f91db8e4039e6995034d9c3e539dd0a83ce7
carlosepmendes <carlosepmendes@gmail.com>
2015-08-03 17:39:35 +0000

    Added database handler

---
+++ app/src/main/java/model/MyWish.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Created by U80030343 on 03-08-2015.
+ */
+public class MyWish {
+
+    public String title;
+    public String content;
+    public String recordDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(String recordDate) {
+        this.recordDate = recordDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
